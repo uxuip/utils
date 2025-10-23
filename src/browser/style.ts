aea6af3ccfcae7ba88a600ee28223999bfa1075b
uxuip <uxuip@outlook.com>
2025-10-23 17:55:13 +0800

    refactor: rename  `getCachedComputedStyle` to `getComputedStyleCached`

--- src/browser/style.ts
+++ src/browser/style.ts
@@ -4,7 +4,7 @@ const computedStyleMap = new WeakMap<Element, CSSStyleDeclaration>()
  * each call of getComputedStyle will return a new object,
  * but it's a live CSS declaration, meaning it'll be updated when the element's style is changed
  */
-export function getCachedComputedStyle(el: Element): CSSStyleDeclaration {
+export function getComputedStyleCached(el: Element): CSSStyleDeclaration {
   let computedStyle: undefined | CSSStyleDeclaration = computedStyleMap.get(el)
   if (!computedStyle) {
     computedStyle = getComputedStyle(el, null)
